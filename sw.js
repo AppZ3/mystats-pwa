ada0c9527f2251d38655b28122bb476bb91d0194
AppZ3 <appzai37@gmail.com>
2026-07-01 21:35:04 +1000

    chore: register pr-detect.js in the service worker cache

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'mystats-v46';
-const ASSETS = ['/', '/index.html', '/style.css', '/manifest.json', '/js/db.js', '/js/profile.js', '/js/config.js', '/js/onboarding.js', '/js/today.js', '/js/workout.js', '/js/running.js', '/js/bodyscan.js', '/js/progress.js', '/js/reminders.js', '/js/settings.js', '/js/app.js', '/js/recovery.js', '/js/journal.js', '/js/prs.js', '/js/programmes.js', '/js/programme-editor.js', '/icon-192.png', '/icon-512.png', '/favicon.ico'];
+const CACHE = 'mystats-v47';
+const ASSETS = ['/', '/index.html', '/style.css', '/manifest.json', '/js/db.js', '/js/profile.js', '/js/config.js', '/js/onboarding.js', '/js/today.js', '/js/workout.js', '/js/running.js', '/js/bodyscan.js', '/js/progress.js', '/js/reminders.js', '/js/settings.js', '/js/app.js', '/js/recovery.js', '/js/journal.js', '/js/prs.js', '/js/programmes.js', '/js/programme-editor.js', '/js/pr-detect.js', '/icon-192.png', '/icon-512.png', '/favicon.ico'];
 
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())));
 self.addEventListener('activate', e => e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim())));
